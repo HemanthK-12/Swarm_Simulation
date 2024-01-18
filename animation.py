54c86904c2e1d3d7e42018ebb96c8c516b8314b8
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2024-01-18 21:44:35 +0530

    updated final draft

--- animation.py
+++ animation.py
@@ -2,10 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# Ask the user for an input number n
-number_of_bots= int(input("Enter a number: "))
-
-# Generate a list of n random coordinates in the range 0 to 50
+number_of_bots= (int)(input("Enter a number: "))
 x = np.random.uniform(0, 50, number_of_bots)
 y = np.random.uniform(0, 50, number_of_bots)
 
@@ -19,55 +16,39 @@ m_bestfit,c_bestfit=bestfit()
 def y_bestfit(x):
     return (m_bestfit*x)+c_bestfit
 
-plt.plot(x,y_bestfit(x),color='b')
-
-# model = LinearRegression()
-# model.fit(x.reshape(-1, 1), y)
-# # Calculate the coefficients of the line equation
-# A = -model.coef_[0]
-# B = 1
-# C = -model.intercept_
-
-# Calculate the intersection points on the best fit line
-# x_intersection = (B*(B*x - A*y) - A*C) / (A**2 + B**2)
-# y_intersection = (A*(-B*x + A*y) - B*C) / (A**2 + B**2)
+bestfit_parameters=np.linspace(0,50,100)
 
 x_intersection = (y+(x/m_bestfit)-c_bestfit)/(m_bestfit+(1/m_bestfit))
 y_intersection = (m_bestfit)*x_intersection+c_bestfit
-
-# Plot the points
-plt.scatter(x, y, color='r')
-
-# Plot the best fit line
-plt.plot(x, y_bestfit(x), color='b')
-
+m_botpath=[]
+c_botpath=[]
 # Plot the perpendicular lines
-for i in range(number_of_bots):
-    plt.plot([x[i], x_intersection[i]], [y[i], y_intersection[i]], color='g')
 
-if(x[i]<x_intersection[i]):
-    x_measure=np.linspace(x[i],x_intersection[i],number_of_bots)
-else:
-    x_measure=np.linspace(x_intersection[i],x[i],number_of_bots)
+# # Define the line
+# m = 1  # slope
+# c = 0  # y-intercept
+# x = np.linspace(0, 10, 100)
+# y = m * x + c
 
-y_measure=(-1/m_bestfit)*x_measure+(y[0]+(x[0]/m_bestfit))
-# Create the figure and the line that we will manipulate
+# # Create the figure and the line that we will manipulate
 fig, ax = plt.subplots()
-line, = ax.plot(x_measure, y_measure, color='k')
+ax.scatter(x,y)
+for i in range(number_of_bots):
+    m_botpath.append((y[i]-y_intersection[i])/(x[i]-x_intersection[i]))
+    c_botpath.append(y[i]-m_botpath[i]*x[i])
+    ax.plot([x[i], x_intersection[i]], [y[i], y_intersection[i]], color='g')
+    #This works because when ax.plot() is given two lists as arguments,it draws a line passing throught the first point and the second point
+#points = ax.scatter([bestfit_parameters[0]], [(y_bestfit(bestfit_parameters))[0]],color='forestgreen')
+points = ax.scatter([x[0]], [y[0]],color='forestgreen')
+one=np.linspace(x[0],x_intersection[0],100)
+two=one*m_botpath[0]+c_botpath[0]
+ax.plot(bestfit_parameters, y_bestfit(bestfit_parameters), color='r')
 
-# The function that makes the animation.
-def update(num, x_measure, y_measure, line):
-    line.set_data(x_measure[:num], y_measure[:num])
-    line.axes.axis([0, 10, 0, 10])
-    return line,
+def update(num,x,y,points):
+    points.set_offsets([x[num],y[num]])
+    return points,
 
-ani = animation.FuncAnimation(fig, update, len(x_measure), fargs=[x_measure, y_measure, line],
+ani = animation.FuncAnimation(fig, update, 100, fargs=[one,two,points],
                               interval=100, blit=True)
 
-# Mark the intersection points
-plt.scatter(x_intersection, y_intersection, color='y')
-
-plt.grid()
 plt.show()
-
-
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,25 +1,12 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
-# Define the line
-m = 1  # slope
-c = 0  # y-intercept
-x = np.linspace(0, 10, 100)
-y = m * x + c
-
-
 # Create the figure and the line that we will manipulate
-fig, ax = plt.subplots()
-line, = ax.plot(x, y, color='k')
+# fig, ax = plt.subplots()
+# line, = ax.plot(bestfit_parameters, y_bestfit(bestfit_parameters), color='b')
 
-# The function that makes the animation.
-def update(num, x, y, line):
-    line.set_data(x[:num], y[:num])
-    line.axes.axis([0, 10, 0, 10])
-    return line,
+# # The function that makes the animation.
+# def update(num, x, y, line):
+#     line.set_data(x[:num], y[:num])
+#     line.axes.autoscale()
+#     return line,
 
-ani = animation.FuncAnimation(fig, update, len(x), fargs=[x, y, line],
-                              interval=100, blit=True)
-
-plt.show()
+# ani = animation.FuncAnimation(fig, update, number_of_bots, fargs=[bestfit_parameters, y_bestfit(bestfit_parameters), line],
+#                               interval=100, blit=True)
